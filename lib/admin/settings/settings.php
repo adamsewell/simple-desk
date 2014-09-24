f58fbefaf3c08a7c2092c843eabc25d0be9e7f17
Adam Sewell <adam@mygeeknc.com>
2014-09-24 00:27:56 -0400

    testing logs

--- lib/admin/settings/settings.php
+++ lib/admin/settings/settings.php
@@ -16,6 +16,15 @@ function sd_display_settings_page(){
 			<div id="tab_container">
 				<form method="post" action="options.php">
 					<?php if ( $active_tab == 'general' ): ?>
+						<?php 
+							$args = array(0, 'error');
+							$logs = WP_Logging::get_logs($args);
+						?>
+						<ul>
+						<?php foreach($logs as $log): ?>
+							<li><?php print_r($log); ?></li>
+						<?php endforeach; ?>
+						</ul>
 						
 					<?php endif; ?>
 
